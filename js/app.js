b292f0edecf582c8eca36049f1a53ead86ead7af
bayers92 <brianc.ayers@gmail.com>
2015-03-12 18:28:04 -0400

    about us section complete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
-
-$(document).ready(function(){
-  $('.slick-photos').slick({
-      dots: true,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 1,
-	  adaptiveHeight: true,
-	  fade: true,
-	  cssEase: 'linear',
-	  arrows: false,
-	  mobileFirst: true
-	});
-});
