dd672b44fe9f98f225c17a216697abc3f4e15c12
bayers92 <brianc.ayers@gmail.com>
2015-03-12 11:26:10 -0400

    first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+  $('.slick-photos').slick({
+      dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  adaptiveHeight: true,
+	  fade: true,
+	  cssEase: 'linear',
+	  arrows: false,
+	  mobileFirst: true
+	});
+});
