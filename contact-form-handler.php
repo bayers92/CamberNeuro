e175571d1c2ad853a7b63d9114f39b7a092ccd14
bayers92 <brianc.ayers@gmail.com>
2015-03-13 14:04:29 -0400

    first round edits

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -17,7 +17,7 @@ if( empty($errors))
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Subject: $subject \n Message: $message"; 
+	" Here are the details:\n Name: $name \n Email: $email \n Subject: $subject \n Message: $message"; 
 
 	
 	mail($to,$email_subject,$email_body,$headers);
