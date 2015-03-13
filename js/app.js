0a2880425b847e15674c604795a4fae29b27d4a6
bayers92 <brianc.ayers@gmail.com>
2015-03-13 02:41:24 -0400

    added hideme class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function() {
+
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.hideme').each( function(i){
+
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			var middle_of_object = $(this).offset().top + 30;
+			var top_of_object = $(this).offset().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			/* If the object is completely visible in the window, fade it it */
+			if( bottom_of_window > middle_of_object ){
+
+				$(this).animate({'opacity':'1'},2000);
+
+			}
+
+		}); 
+
+	});
+
+});
