ae3e637db3110e6188a292ffa066e81e0e080274
bayers92 <brianc.ayers@gmail.com>
2015-03-20 17:15:15 -0400

    final first iteration complete

---
+++ vivus_script.js
@@ -0,0 +1,6 @@
+// Insert this into bottom of html file
+
+<script src="js/vivus.js"></script>
+<script>
+var hi = new Vivus('hi-there', {type: 'scenario-sync', duration: 20, start: 'autostart', dashGap: 20, forceRender: false});
+</script>
